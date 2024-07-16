97fc50cd29c136da6b34e8bf910e836ad1f5b142
FaisalEganow <faisal@eganow.com>
2024-07-16 10:13:12 +0000

    code clean up

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { Suspense, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Donate from './components/Donate';
